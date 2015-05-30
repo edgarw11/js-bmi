4a1778a0133cdbbc132e1565bcfe4446448bcb4d
edgarw11 <edgarw11@gmail.com>
2015-05-30 16:36:17 -0300

    Implement Controller

--- asset/js/BMIService.js
+++ asset/js/BMIService.js
@@ -1,8 +1,11 @@
 var BMIService = {
     
-    getIndex: function(weight, height){
-        return weight / (height * height);
-    },
+    getIndex: function(weight, height) {
+		if(typeof(weight) === 'number' && typeof(height) === 'number') {
+			return weight / (height * height);
+		}
+		return null;
+	},
     
     getDescription: function(index){
         //TODO to be implemented
